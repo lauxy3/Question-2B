6ae4777a343d8b08c80e4d048dbe5426c9c178f7
lauxy1 <lauxy1work@gmail.com>
2022-04-25 14:45:53 +0800

    source code for producer

---
+++ producer/Message.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace producer.Models
+{
+    public class Message
+    {
+        public string Email { get; set; }
+        public string password {get; set; }
+        public string task {get; set;}
+
+
+    }
+}
